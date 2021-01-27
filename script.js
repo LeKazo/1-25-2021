7ce020e0018fbe3b57863d56b3ade48d04ca8c47
LeKazo <70152390+LeKazo@users.noreply.github.com>
2021-01-27 08:17:06 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let dog = {
+    type: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return "This dog has " + dog.numLegs +"legs"}
+};
+
+dog.sayLegs();
+
+
+console.log(dog.sayLegs(4));                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
+document.write(console.log(dog.sayLegs));
